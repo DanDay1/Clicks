cac91e5035ea85197e22216bcbe067a391382f92
Daniel Day <danielday@Daniels-MacBook-Pro.local>
2018-04-24 21:51:08 -0400

    working on timer

--- clicks.js
+++ clicks.js
@@ -4,21 +4,52 @@
 // var day = moment.unix(1318781876);
 // console.log ("Day: " + day);
 
+var clickcount = 0;
+console.log(clickcount);
+
+var a = moment();
+var b = moment();
+
+var result = 0;
+var time = "never";
+console.log("result: " + result);
+console.log("time: " + time);
+
+
+
+
+//What I think I need to do is initialive variables and cover if statements for if no time, display "never", then execute the click function. 
+
+
 function clickCounter() {
-    if(typeof(Storage) !== "undefined") {
+    if (typeof(Storage) !== "undefined") {
         if (localStorage.clickcount) {
-            localStorage.clickcount = Number(localStorage.clickcount)+1;
+            localStorage.clickcount = Number(localStorage.clickcount) + 1;
         } else {
-            localStorage.clickcount = 1;
+            localStorage.clickcount = 0;
         }
-        document.getElementById("result").innerHTML = "You have clicked the button " + localStorage.clickcount + " time(s).";
+        //if (localStorage.lastclick) {
+           
+             localStorage.lastclick = b;
+            //localStorage.lastclick = Number(localStorage.clickcount) + 10;
+        //} 
+         //else {
+         //    localStorage.lastclick = 0;
+         //}
+        document.getElementById("result").innerHTML = localStorage.clickcount + " time(s).";
     } else {
         document.getElementById("result").innerHTML = "Sorry, your browser does not support web storage...";
     }
 
-document.getElementById("result").innerHTML = "Last clicked " + moment().format("hh:mm A");
+    document.getElementById("time").innerHTML = a.format("hh:mm A") + " " + a.diff(b, 'minutes') + " minutes ago";
 
-console.log(moment().format("hh:mm A"));
-console.log(moment().startOf('minute').fromNow());  
+    // document.getElementById("time").innerHTML = moment().format("hh:mm A") + " " + moment().startOf('').fromNow();
+    console.log("clickcount: " + localStorage.clickcount);
+    console.log("lastclick: " + localStorage.lastclick);
+    console.log("result: " + result);
+    console.log("time: " + time);
+    console.log(moment().format("hh:mm A"));
 
 }
+
+console.log(moment().startOf('hour').fromNow());
