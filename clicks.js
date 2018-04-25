8b4a48601f162daba3a8a1b1611e0a523df492fb
Daniel Day <danielday@Daniels-MacBook-Pro.local>
2018-04-24 22:28:42 -0400

    Cleaned up code, added comment on what still needs to get done

--- clicks.js
+++ clicks.js
@@ -1,8 +1,4 @@
-// var moment = require('moment');
-// moment().format();
-
-// var day = moment.unix(1318781876);
-// console.log ("Day: " + day);
+//There are two tasks that I still need to finish to complete this assignment. The first is to get the initial variables to display when the page loads. To get that to happen, I know I'll need to initialize the variables and display them on the page load. Right now, the reason why they don't appear until the button clicks is because they all fall under the click function. The second task that still needs to get done is to get the elapsed time to refresh. To get the elapsed time to refresh, I know I need to implement some form of timer that would compare the current time with the last clicked time every minute. I already set the comparison function on line 29.
 
 var clickcount = 0;
 console.log(clickcount);
@@ -15,12 +11,6 @@ var time = "never";
 console.log("result: " + result);
 console.log("time: " + time);
 
-
-
-
-//What I think I need to do is initialive variables and cover if statements for if no time, display "never", then execute the click function. 
-
-
 function clickCounter() {
     if (typeof(Storage) !== "undefined") {
         if (localStorage.clickcount) {
@@ -28,14 +18,9 @@ function clickCounter() {
         } else {
             localStorage.clickcount = 0;
         }
-        //if (localStorage.lastclick) {
-           
-             localStorage.lastclick = b;
-            //localStorage.lastclick = Number(localStorage.clickcount) + 10;
-        //} 
-         //else {
-         //    localStorage.lastclick = 0;
-         //}
+
+        localStorage.lastclick = b;
+
         document.getElementById("result").innerHTML = localStorage.clickcount + " time(s).";
     } else {
         document.getElementById("result").innerHTML = "Sorry, your browser does not support web storage...";
@@ -43,12 +28,18 @@ function clickCounter() {
 
     document.getElementById("time").innerHTML = a.format("hh:mm A") + " " + a.diff(b, 'minutes') + " minutes ago";
 
-    // document.getElementById("time").innerHTML = moment().format("hh:mm A") + " " + moment().startOf('').fromNow();
+
     console.log("clickcount: " + localStorage.clickcount);
     console.log("lastclick: " + localStorage.lastclick);
     console.log("result: " + result);
     console.log("time: " + time);
     console.log(moment().format("hh:mm A"));
+
+    moment.fn.minutesFromNow = function() {
+        return Math.floor((+new Date() - (+this)) / 60000) + ' mins ago';
+    }
+    //then call:
+    moment.unix(d).minutesFromNow();
 
 }
 
